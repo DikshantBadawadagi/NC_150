11e10b4f49e06c9ddd77978bee46ac46dda319b1
DikshantBadawadagi <153322418+DikshantBadawadagi@users.noreply.github.com>
2025-06-01 23:57:37 +0530

    Create partitionEqSub.cpp

---
+++ partitionEqSub.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool canPartition(vector<int>& nums) {
+        int sum = accumulate(nums.begin(), nums.end(), 0);
+        if (sum & 1) return false;
+        int target = sum / 2;
+        bitset<20001> bits;  // adjust the 20001 to max possible target+1
+        bits[0] = 1;
+        for (int num : nums) {
+            bits |= (bits << num);
+            if (bits[target]) return true;
+        }
+        return bits[target];
+    }
+};
